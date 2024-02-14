69befbb8d563523d1d9770523a93acc4c585248a
dens1ne <i@djantonjuk.ru>
2024-02-14 17:35:33 +0300

    Список профессий

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ def training(prof: str):
     return render_template('training.html', title='НУЖНО БОЛЬШЕ ТРЕНИРОВАТЬСЯ', prof=prof)
 
 
+@app.route('/list_prof/<ltype>')
+def list_prof(ltype: str):
+    profs = ''' менеджер по продажам,
+                продавец-консультант,
+                водитель,
+                бухгалтер,
+                программист, разработчик программного обеспечения,
+                врач,
+                инженер,
+                повар,
+                упаковщик и комплектовщик,
+                слесарь,
+                сантехник'''
+    profs = profs.strip().split(',')
+    return render_template(
+        'prof_list.html', title='Профессии, которым НУЖНО БОЛЬШЕ ТРЕНИРОВАТЬСЯ', profs=profs, ltype=ltype)
+
+
 if __name__ == '__main__':
     app.config['DEBUG'] = True
     app.run(port=8080, host='127.0.0.1')
