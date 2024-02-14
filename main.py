fd58cdd5f9f370b0875fd94ae2d0aa7525386991
dens1ne <i@djantonjuk.ru>
2024-02-14 16:43:11 +0300

    Готовимся к миссии

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title: str = 'Миссия Колонизация Марса'):
+    return render_template('index.html', title=title)
+
+
+if __name__ == '__main__':
+    app.config['DEBUG'] = True
+    app.run(port=8080, host='127.0.0.1')
