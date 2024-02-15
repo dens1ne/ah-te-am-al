95ff13fb439393f2f4099fcdb9f4e64bc0ac7cc5
dens1ne <i@djantonjuk.ru>
2024-02-15 20:15:50 +0300

    Галерея с добавлением

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+urls = ['https://million-wallpapers.ru/wallpapers/4/37/10737825692406921179/bolshoe-more-krasivyj-zakat.jpg',
+        'https://gas-kvas.com/uploads/posts/2023-02/1675496354_gas-kvas-com-p-oboi-na-rabochii-stol-dlya-fonovogo-risunk-30.jpg',
+        'https://wallpapers.com/images/hd/best-background-q6yyd1kpbb841wyl.jpg']
 
 
 @app.route('/<title>')
@@ -60,6 +63,23 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Доступ')
 
 
+"""
+мистэ прэзэднт вот а ю финк эбаут дииз натс?..
+диз- натс? хе хэд но шэнс, ноу шэнс эд олл. хи майт кол химселф натс бат хи ивен хэд нот болз.
+лайф эинт изи фо э  биилльаниа сан: ю нид то ворк хард то гет а лилл сам-сам .
+хэй, ду ю финк ай м дамб? ай форгот, зере воз самфинг абаут джамп
+ай лав чайна! зей дид самфинг грэйт бифо! тугезер, америка, ви вил билд а грэйтер вол.
+хэллоу, айм электед, ай вилл мэйк ас грейт. ай стартед фром зе ботом оф май фазерс товерс виз
+э смол лоан оф а уан биллион даларс. ит воз а страгл. йэ.
+ййэ
+фэнкс фор электинг ми, хере из йоур квортер "эвсом!"
+
+фол ол зе лузеес энд хэйтерс ху сэй ай вор э... воревиг
+
+энд бикоз оф зет некст стэйдж оф афэйрс вил инклюд йоур маза
+"""
+
+
 @app.route('/login')
 def login():
     form = LoginForm()
@@ -82,6 +102,17 @@ def destribution():
 @app.route('/table/<sex>/<int:age>')
 def table(sex: str, age: int):
     return render_template('mars_table.html', title='Оформление кают', sex=sex, age=age)
+
+
+@app.route('/galery', methods=['GET', 'POST'])
+def galery():
+    if request.method == 'POST':
+        f = request.files['file']
+        with open(f'static/img/test_image_{len(urls) - 3}.png', 'wb') as img_file:
+            img_file.write(f.read())
+        urls.append(f'static/img/test_image_{len(urls) - 3}.png')
+
+    return render_template('galery.html', title='Галерея с добавлением', urls=urls)
 
 
 if __name__ == '__main__':
