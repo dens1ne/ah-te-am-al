b726abe07988b207c9f23b68f317da3ae4113f6a
Максим Астахов <mastahov112@gmail.com>
2024-03-06 17:48:21 +0300

    Тестирование удаления

--- data/tests/test.py
+++ data/tests/test.py
@@ -1,17 +1,10 @@
-from requests import get, post
-
-# Empty request
-print(post('http://localhost:5000/api/jobs', json={}).json())
-
-# Bad request
-print(post('http://localhost:5000/api/jobs', json={'team_leader_id': 'some id'}).json())
+from requests import delete
 
 # Correct request
-print(post('http://localhost:5000/api/jobs',
-           json={'team_leader_id': 1,
-                 'job': 'Recovery of modules 5 and 6',
-                 'work_size': 30,
-                 'start_date': '2024-01-01 00:00:00.000000',
-                 'end_date': '2024-02-01 00:00:00.000000',
-                 'is_finished': True,
-                 'collaborators': '2,3,4'}).json())
+print(delete('http://localhost:5000/api/jobs/3').json())
+
+# Wrong id
+print(delete('http://localhost:5000/api/jobs/5000').json())
+
+# Wrong type of parameter
+print(delete('http://localhost:5000/api/jobs/wrongtype').json())
